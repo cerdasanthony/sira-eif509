71d65ea0f80ffb27f975dd57f5a146a58e23ad3c
Anthony Cerdas Chacon <anthony.cerdas.chacon@est.una.ac.cr>
2026-08-10 19:21:40 -0600

    chore: proyecto base Spring Boot 3.3 + Gradle sobre Java 21
    Salida de Spring Initializr con Spring Web y Actuator. Se versiona el
    wrapper de Gradle para que el proyecto compile sin instalar Gradle.

---
+++ src/test/java/cr/ac/una/sira/SiraApplicationTests.java
@@ -0,0 +1,13 @@
+package cr.ac.una.sira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
